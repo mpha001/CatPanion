f2534f3080b6f5d9b746969c40ab21b2b01635e7
mpha001 <minypham95@gmail.com>
2025-03-17 21:53:31 -0400

    add new tracks

--- src/components/musicPlayer/musicPlayer.jsx
+++ src/components/musicPlayer/musicPlayer.jsx
@@ -1,12 +1,15 @@
 import "./musicPlayer.scss";
 import AudioPlayer from "react-h5-audio-player";
 import LofiTrack from "../../assets/audio/lofi_track_1.mp3";
+import LofiTrack2 from "../../assets/audio/lofi_track_2.mp3";
+import LofiTrack3 from "../../assets/audio/lofi_track_3.mp3";
 import "react-h5-audio-player/src/styles.scss";
 
 function musicPlayer() {
+  const playlist = [{}];
   return (
-    <div className='audio-container'>
-      <AudioPlayer src={LofiTrack} onPlay={e => console.log("onPlay")} />
+    <div className="audio-container">
+      <AudioPlayer src={LofiTrack} onPlay={(e) => console.log("onPlay")} />
     </div>
   );
 }
